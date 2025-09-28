a1709bfdb294fbfd857d94cc9a1006152cc3c124
Anup <anup21panchal@gmail.com>
2025-09-28 19:55:53 +0530

    test commit

--- src/main/java/com/hotel/hotel_booking_service/HotelBookingServiceApplication.java
+++ src/main/java/com/hotel/hotel_booking_service/HotelBookingServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelBookingServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(HotelBookingServiceApplication.class, args);
 	}
